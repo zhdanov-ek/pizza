add3ae05518325c807ddf5f6b72ac3f4b6f38bb2
Zhdanov Evgeniy <zhdanov.ek@gmail.com>
2017-03-05 01:09:43 +0200

    add resources, testing basic operations of drawing

--- app/src/main/java/com/example/gek/pizza/activities/MakePizzaActivity.java
+++ app/src/main/java/com/example/gek/pizza/activities/MakePizzaActivity.java
@@ -1,12 +1,18 @@
 package com.example.gek.pizza.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.gek.pizza.R;
 import com.example.gek.pizza.data.Connection;
@@ -15,6 +21,9 @@ import com.example.gek.pizza.data.Const;
 /** Создание своей пиццы */
 
 public class MakePizzaActivity extends BaseActivity {
+
+    private int nextIngredient = 0;
+    private ImageView ivPizza;
 
     @Override
     public void updateUI() {
@@ -26,7 +35,10 @@ public class MakePizzaActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_make_pizza);
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(this.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_make_pizza, null, false);
+        mDrawer.addView(contentView, 0);
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
@@ -39,7 +51,35 @@ public class MakePizzaActivity extends BaseActivity {
         mDrawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        ivPizza = (ImageView) findViewById(R.id.ivPizza);
+        findViewById(R.id.btnIngredient).setOnClickListener(listenerComponent);
+
     }
+
+    View.OnClickListener listenerComponent = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            // Наши картинки в отдельных битмапах
+            Bitmap basis = BitmapFactory.decodeResource(getResources(), R.drawable.pizza_basis);
+            Bitmap ananas = BitmapFactory.decodeResource(getResources(), R.drawable.pizza_ananas);
+            Bitmap mushroomsFried = BitmapFactory.decodeResource(getResources(), R.drawable.pizza_mushrooms_fried);
+
+            // Создаем канвас через который будем рисовать на нашей главной битмапе - основе
+            Canvas canvas = new Canvas(basis);
+
+            // Добавляем в главную битмапу через канву дополнительные картинки каждого ингредиента
+            canvas.drawBitmap(basis, 0, 0, null);
+            canvas.drawBitmap(ananas, 0, 0, null);
+            canvas.drawBitmap(mushroomsFried, 0, 0, null);
+
+            // Выводим картинку во вью
+            ivPizza.setImageBitmap(basis);
+
+        }
+    };
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
