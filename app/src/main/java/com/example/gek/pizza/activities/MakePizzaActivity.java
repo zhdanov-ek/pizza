f523a090083b48607eb8aec3f890a0aba685b10f
Zhdanov Evgeniy <zhdanov.ek@gmail.com>
2017-03-05 23:37:39 +0200

    work drag&drop, simple list ingredients

--- app/src/main/java/com/example/gek/pizza/activities/MakePizzaActivity.java
+++ app/src/main/java/com/example/gek/pizza/activities/MakePizzaActivity.java
@@ -1,29 +1,42 @@
 package com.example.gek.pizza.activities;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.gek.pizza.R;
 import com.example.gek.pizza.data.Connection;
 import com.example.gek.pizza.data.Const;
 
+import java.util.ArrayList;
+
 /** Создание своей пиццы */
 
 public class MakePizzaActivity extends BaseActivity {
 
-    private int nextIngredient = 0;
+    public static final String TAG = "3333333333";
+
+    private RelativeLayout rlContent;
+    private LinearLayout llIngredients;
     private ImageView ivPizza;
+    private ArrayList<Integer> listIngredients;
+    private ImageView currentIngredient;
+
 
     @Override
     public void updateUI() {
@@ -40,7 +53,6 @@ public class MakePizzaActivity extends BaseActivity {
         View contentView = inflater.inflate(R.layout.activity_make_pizza, null, false);
         mDrawer.addView(contentView, 0);
 
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
@@ -51,31 +63,126 @@ public class MakePizzaActivity extends BaseActivity {
         mDrawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        llIngredients = (LinearLayout) findViewById(R.id.llIngredients);
+        rlContent = (RelativeLayout) findViewById(R.id.rlContent);
         ivPizza = (ImageView) findViewById(R.id.ivPizza);
-        findViewById(R.id.btnIngredient).setOnClickListener(listenerComponent);
 
+        findViewById(R.id.btnClear).setOnClickListener(listenerClear);
+
+        // cлушаем события перетягивания на нашу пиццу
+        ivPizza.setOnDragListener(onDragListenerIngredient);
+
+        clearPizza();
     }
 
-    View.OnClickListener listenerComponent = new View.OnClickListener() {
+    View.OnLongClickListener ingredientLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View view) {
+            Log.d(TAG, "onLongClick: ");
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+            view.startDrag(data, shadowBuilder, view, 0);
+            currentIngredient = (ImageView) view;
+            return true;
+        }
+    };
+
+//    View.OnTouchListener ingredientTouchListener = new View.OnTouchListener() {
+//        @Override
+//        public boolean onTouch(View view, MotionEvent motionEvent) {
+//
+//            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+//                ClipData data = ClipData.newPlainText("", "");
+//                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+//                view.startDrag(data, shadowBuilder, view, 0);
+//                view.setVisibility(View.GONE);
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        }
+//    };
+
+    private void clearPizza(){
+        if (listIngredients == null) {
+            listIngredients = new ArrayList<>();
+        } else {
+            listIngredients.clear();
+        }
+
+        llIngredients.removeAllViews();
+
+        for (int i = 0; i < 5; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setId(i);
+            imageView.setPadding(2, 2, 2, 2);
+            imageView.setImageBitmap(
+                    BitmapFactory.decodeResource(getResources(), R.drawable.pizza_list_tomato));
+            imageView.setOnLongClickListener(ingredientLongClickListener);
+            llIngredients.addView(imageView);
+        }
+        updatePizza();
+    }
+
+    private void updatePizza(){
+        Bitmap basis = BitmapFactory.decodeResource(getResources(), R.drawable.pizza_basis);
+
+        // Создаем канвас через который будем рисовать на нашей битмапе - result
+        Bitmap result = Bitmap.createBitmap(basis.getWidth(), basis.getHeight(), basis.getConfig());
+        Canvas canvas = new Canvas(result);
+
+        // Выводим первый слой - основу пиццы
+        canvas.drawBitmap(basis, 0, 0, null);
+
+        // выводим все слои с массива - выбранные ингредиенты
+        for (int resource: listIngredients){
+            Bitmap layer = BitmapFactory.decodeResource(getResources(), resource);
+            canvas.drawBitmap(layer, 0, 0, null);
+        }
+
+        // Выводим картинку во вью
+        ivPizza.setImageBitmap(result);
+    }
+
+    private View.OnClickListener listenerClear = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            clearPizza();
+        }
+    };
 
-            // Наши картинки в отдельных битмапах
-            Bitmap basis = BitmapFactory.decodeResource(getResources(), R.drawable.pizza_basis);
-            Bitmap ananas = BitmapFactory.decodeResource(getResources(), R.drawable.pizza_ananas);
-            Bitmap mushroomsFried = BitmapFactory.decodeResource(getResources(), R.drawable.pizza_mushrooms_fried);
 
-            // Создаем канвас через который будем рисовать на нашей главной битмапе - основе
-            Canvas canvas = new Canvas(basis);
+    // Отрабатываем события во время перетягивания
+    private View.OnDragListener onDragListenerIngredient = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View view, DragEvent dragEvent) {
+            switch (dragEvent.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    Log.d(TAG, "Action is DragEvent.ACTION_DRAG_STARTED");
+                    break;
 
-            // Добавляем в главную битмапу через канву дополнительные картинки каждого ингредиента
-            canvas.drawBitmap(basis, 0, 0, null);
-            canvas.drawBitmap(ananas, 0, 0, null);
-            canvas.drawBitmap(mushroomsFried, 0, 0, null);
+                case DragEvent.ACTION_DRAG_LOCATION:
+                    float X = dragEvent.getX();
+                    float Y = dragEvent.getY();
+                    Log.d(TAG, "X " + (int) X + " Y " + (int) Y);
+                    break;
 
-            // Выводим картинку во вью
-            ivPizza.setImageBitmap(basis);
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    Log.d(TAG, "Action is DragEvent.ACTION_DRAG_ENTERED");
+                    break;
 
+                case DragEvent.ACTION_DROP:
+                    currentIngredient.setVisibility(View.GONE);
+                    listIngredients.add(R.drawable.pizza_bacon);
+                    listIngredients.add(R.drawable.pizza_tomatoes);
+                    updatePizza();
+                    Log.d(TAG, "Action is DragEvent.ACTION_DRAG_DROPPED");
+                    break;
+
+                default:
+                    break;
+            }
+            return true;
         }
     };
 
